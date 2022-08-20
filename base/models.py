e368e655fefbb336e19c05ca818ed7d1c8422a6e
danibrogue <hotlinetheslayer@gmail.com>
2022-08-20 11:34:10 +0300

    models config

--- base/models.py
+++ base/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Category(models.Model):
+    title = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.title
+
+
+class Article(models.Model):
+    headline = models.CharField(max_length=30)
+    text = models.TextField(max_length=5000)
+    publish_date = models.DateTimeField(auto_now=True)
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True)
+
+    def __str__(self):
+        return self.headline
