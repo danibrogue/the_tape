9227cfd8c5c2d690e86fe56ede177c89401fb379
danibrogue <hotlinetheslayer@gmail.com>
2022-08-21 09:18:08 +0300

    index and CRUD views created

--- base/views.py
+++ base/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+
+def index(request):
+    return HttpResponse('homepage')
+
+
+def new(request):
+    return HttpResponse('new article')
+
+
+def show(request):
+    return HttpResponse('show article')
+
+
+def edit(request):
+    return HttpResponse('edit article')
+
+
+def delete(request):
+    return HttpResponse('delete article')
